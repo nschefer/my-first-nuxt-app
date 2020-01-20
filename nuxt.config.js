feb26f2b8a09e40bd4dff2864b0c54da4739e071
Noah Schefer <noahschefer@Noahs-MacBook-Pro.local>
2020-01-20 17:46:55 -0500

    Lesson 15 Complete

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
+    // '@nuxtjs/eslint-module'
   ],
   /*
   ** Nuxt.js modules
@@ -48,7 +48,7 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,18 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        my-first-nuxt-app
-      </h1>
-      <h2 class="subtitle">
-        My shining Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <Logo />
+    <nuxt-link to="/users">Users</nuxt-link>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from '~/components/Logo.vue';
 
 export default {
   components: {
-    Logo
-  }
-}
+    Logo,
+  },
+};
 </script>
 
 <style>
