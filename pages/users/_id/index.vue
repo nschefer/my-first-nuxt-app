14fe4a72c7a00320e3fe3b4dc5764d5371915b79
Noah Schefer <noahschefer@Noahs-MacBook-Pro.local>
2020-01-21 10:17:33 -0500

    Lesson 16 Complete

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,3 +1,11 @@
 <template>
   <h1>A Single User with ID: {{ $route.params.id }}</h1>
 </template>
+
+<script>
+export default {
+  validate(data) {
+    return data.params.id == 1;
+  },
+};
+</script>
